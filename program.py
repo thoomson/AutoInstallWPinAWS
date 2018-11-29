7102aa9af635e74c2be0506e833eeb3ebccd6b72
Thomson33 <ant.vente@gmail.com>
2018-11-29 18:05:37 +0100

    Style: add a function to send sms

--- program.py
+++ program.py
@@ -5,7 +5,7 @@ Used Apache2, PHP 7.0, MySQL and Let's encrypt (SSL is optionnal)
 
 """
 
-#Libraries
+# Libraries
 import time
 
 import boto3
@@ -14,12 +14,12 @@ import paramiko
 from functions import *
 from variables import *
 
-#Beginning of the program
+# Beginning of the program
 url = input('Enter the URL as \'example.com\' (without \'www\' or \'http(s)\'): ')
 
 ssl = input('Do you want an HTTPS website ? (o/n): ').lower() # HTTPS is powered by LetsEncrypt
 
-#Create the boto3 ressource
+# Create the boto3 ressource
 ec2 = boto3.resource(
 	'ec2', 
 	aws_access_key_id = AWS_ACCESS_KEY,
@@ -64,6 +64,7 @@ install_apache2(ssh, sftp, url)
 # Install SSL certificate if desired
 if ssl == 'o':
 	install_ssl(ssh, sftp, url, ip)
+	auto_renew_ssl(ssh, sftp, url)
 
 # Install and configure PHP
 install_php(ssh)
@@ -81,13 +82,7 @@ print('IP of your machine : ' + ip)# Display the public IP of the AWS instance
 print('The installation is finished !')
 
 # Send SMS
-sms = boto3.client(
-	"sns",
-	aws_access_key_id = AWS_SNS_ACCESS_KEY,
-	aws_secret_access_key = AWS_SNS_SECRET_KEY,
-	region_name = AWS_SNS_REGION_NAME
-	)
-
-sms.publish(PhoneNumber=YOUR_PHONE_NUMBER, Message="Your Wordpress site on EC2 is installed ! Go to it on : {}".format(ip))
+message = "Your Wordpress site on EC2 is installed ! Go to it on : {} ({})".format(url, ip)
+send_sms(message, YOUR_PHONE_NUMBER)
 
 # End of the program
